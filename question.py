99c0ccc2f9ee5a9c10a89cd30ad67bb73c069f24
Peter Holloway <holloway.p.r@gmail.com>
2017-09-30 21:47:40 +0100

    Update demo questions to show multiline display

--- question.py
+++ question.py
@@ -4,5 +4,6 @@
 from answers import prompts
 
 if __name__ == "__main__":
-    for i in range(3):
-        c = prompts.choice('Prompt {}: '.format(i), ['abcd', 'efgh', 'ijkl', 'mnop'])
+    c = prompts.choice('Choose an option\nwith detail on second line', ['option 1', 'option 2', 'option 2'])
+    c = prompts.choice('Choose a thing', ['thing 1\npart 1', 'thing 1\npart2 2', 'thing 2', 'thing 3'])
+    c = prompts.choice('Finally choose some letters', ['abcd', 'efgh', 'ijkl', 'mnop'])
