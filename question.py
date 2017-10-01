f9bb5fabcdafffc3169861b7a6ba701993019653
Peter Holloway <holloway.p.r@gmail.com>
2017-10-01 15:43:52 +0100

    Update questions demo to print selection
    choice method no longer prints results

--- question.py
+++ question.py
@@ -4,6 +4,11 @@
 from answers import prompts
 
 if __name__ == "__main__":
-    c = prompts.choice('Choose an option\nwith detail on second line', ['option 1', 'option 2', 'option 2'])
-    c = prompts.choice('Choose a thing', ['thing 1\npart 1', 'thing 1\npart2 2', 'thing 2', 'thing 3'])
+    c = prompts.choice('Choose an option', ['option 1', 'option 2', 'option 3'])
+    print('Option: {}'.format(c))
+    c = prompts.choice('Choose a thing', ['thing 1 part 1', 'thing 1\npart 2', 'thing 2', 'thing 3'])
+    print('Thing {}'.format(c))
+    c = prompts.choice('Pick a number', [1234, 4238, 43230, 209348], multi=False)
+    print('Number: {}'.format(c))
     c = prompts.choice('Finally choose some letters', ['abcd', 'efgh', 'ijkl', 'mnop'])
+    print('Letters: {}'.format(', '.join(c)))
