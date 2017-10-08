156dc462ca172f07ac7793443b23113c62fd67dd
Peter Holloway <holloway.p.r@gmail.com>
2017-10-08 19:25:20 +0100

    Add more tests for Document

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,15 +1,28 @@
+import answers
 import pytest
+from unittest import mock
 
 @pytest.fixture
 def doc():
-    import answers
     return answers.Document()
+
+sample_text = """this is a piece of sample text containing varying
+line
+lengths.
+
+And new paragraphs also with multiple lines"""
+
+# @pytest.fixture
+# def sample_text():
+#     return sample_text
 
 @pytest.fixture
 def full_doc():
-    import answers
-    return answers.Document('this is a paragraph\nof text')
+    return answers.Document(sample_text)
 
+@pytest.fixture
+def mock_doc():
+    return mock.Mock()
 
 def test_add_keys(doc):
     doc.add('a')
@@ -22,7 +35,31 @@ def test_home(full_doc):
 
 def test_bksp(full_doc):
     full_doc.bksp()
-    assert str(full_doc) == 'this is a paragraph\nof tex'
+    assert str(full_doc) == sample_text[:-1]
     full_doc.bksp()
-    assert str(full_doc) == 'this is a paragraph\nof te'
+    assert str(full_doc) == sample_text[:-2]
+
+def test_handle_literal(mock_doc):
+    answers.Document.handle(mock_doc, 'a')
+    mock_doc.add.assert_called_once_with('a')
+    answers.Document.handle(mock_doc, '%')
+    mock_doc.add.assert_called_with('%')
+
+def test_handle_bksp(mock_doc):
+    answers.Document.handle(mock_doc, '<BACKSPACE>')
+    mock_doc.bksp.assert_called_once_with()
+
+def test_handle_newline(mock_doc):
+    answers.Document.handle(mock_doc, '<Ctrl-j>')
+    mock_doc.add.assert_called_once_with('\n')
+
+def test_lines(full_doc):
+    lines = full_doc.lines
+    assert len(lines) == 5
+
+def test_initial_cursor(full_doc):
+    cursor = full_doc.cursor
+    assert cursor[0] == 4
+    assert cursor[1] == 43
+
 
