745743710088b22041188b697d6bc599c013a33c
Peter Holloway <holloway.p.r@gmail.com>
2017-10-15 21:58:53 +0100

    Use direct imports for document tests

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,21 +1,22 @@
-import answers
+from answers import Document
+from answers import document
 import pytest
 from unittest import mock
 
 @pytest.fixture
 def doc():
-    return answers.Document()
+    return Document()
 
 sample_text = """this is a piece of sample text containing varying
 line
 lengths.
 
-And new paragraphs also with multiple lines"""
+And new paragraphs also with     multiple lines"""
 
 
 @pytest.fixture
 def full_doc():
-    return answers.Document(sample_text)
+    return Document(sample_text, 58)
 
 @pytest.fixture
 def mock_doc():
@@ -30,22 +31,22 @@ def test_add_keys(doc):
 
 def test_bksp(full_doc):
     full_doc.bksp()
-    assert str(full_doc) == sample_text[:-1]
+    assert str(full_doc) == sample_text[:57] + sample_text[58:]
     full_doc.bksp()
-    assert str(full_doc) == sample_text[:-2]
+    assert str(full_doc) == sample_text[:56] + sample_text[58:]
 
 def test_handle_literal(mock_doc):
-    answers.Document.handle(mock_doc, 'a')
+    Document.handle(mock_doc, 'a')
     mock_doc.add.assert_called_once_with('a')
-    answers.Document.handle(mock_doc, '%')
+    Document.handle(mock_doc, '%')
     mock_doc.add.assert_called_with('%')
 
 def test_handle_bksp(mock_doc):
-    answers.Document.handle(mock_doc, '<BACKSPACE>')
+    Document.handle(mock_doc, '<BACKSPACE>')
     mock_doc.bksp.assert_called_once_with()
 
 def test_handle_newline(mock_doc):
-    answers.Document.handle(mock_doc, '<Ctrl-j>')
+    Document.handle(mock_doc, '<Ctrl-j>')
     mock_doc.add.assert_called_once_with('\n')
 
 def test_lines(full_doc):
@@ -54,20 +55,20 @@ def test_lines(full_doc):
 
 def test_initial_cursor(full_doc):
     cursor = full_doc.cursor
-    assert cursor[0] == 4
-    assert cursor[1] == 43
+    assert cursor[0] == 2
+    assert cursor[1] == 3
 
 def test_jump_left_word(full_doc):
-    full_doc.move_word(answers.document.Dir.LEFT)
+    full_doc.move_word(document.Dir.LEFT)
     cursor = full_doc.cursor
     assert str(full_doc) == sample_text
-    assert cursor[0] == 4
-    assert cursor[1] == 38
+    assert cursor[0] == 2
+    assert cursor[1] == 0
 
 def test_jump_right_word(full_doc):
-    full_doc.move_word(answers.document.Dir.RIGHT)
+    full_doc.move_word(document.Dir.RIGHT)
     cursor = full_doc.cursor
     assert str(full_doc) == sample_text
-    assert cursor[0] == 4
-    assert cursor[1] == 43
+    assert cursor[0] == 2
+    assert cursor[1] == 7
 
