6726bb56ae35cedda3ddd893715d1e3b5246601e
Peter Holloway <holloway.p.r@gmail.com>
2018-03-04 16:33:52 +0000

    Restrict the modules available after importing answers

--- answers/__init__.py
+++ answers/__init__.py
@@ -2,3 +2,7 @@ __author__ = 'Peter Holloway'
 from answers.yesno import confirm
 from answers.choices import choose
 from answers.document import prompt as freetext
+
+__all__ = ['confirm', 'choose', 'freetext']
+
+del yesno, choices, document
