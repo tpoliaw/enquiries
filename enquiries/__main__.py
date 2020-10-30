871ab75ec6083d1d0eba858c36673dfb7191733f
Peter Holloway <holloway.p.r@gmail.com>
2020-10-30 09:31:25 +0000

    Convert from click to argparse
    Click was nice but was an unnecessary dependency given script
    use isn't the main purpose of this library.
    
    Fixes #11

--- enquiries/__main__.py
+++ enquiries/__main__.py
@@ -1,55 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
+import sys
 
 from enquiries import choose
 from enquiries import freetext
 from enquiries import yesno
-import sys
-import click
-
-@click.group()
-def cli():
-    pass
 
 
-@cli.command()
-@click.option('-p', '--prompt', default='enter text: ')
-@click.option('-q', '--quiet', is_flag=True)
-def free(prompt, quiet):
-    user_input = freetext(prompt)
+def free(args):
+    user_input = freetext(args.prompt or '')
     # If we're not being quiet and stdout is not being redirected, write the prompt
-    if not quiet and sys.stdout.isatty():
-        click.secho(prompt, err=True, bold=True)
+    if args.prompt and not args.quiet and sys.stdout.isatty():
+        print(args.prompt or '', file=sys.stderr)
     # always print the output
-    click.echo(user_input)
+    print(user_input)
 
 
-@cli.command()
-@click.option('-m', '--multiple-choice', is_flag=True, help='User can choose multiple options')
-@click.option('-p', '--prompt', help='The message to display to the user')
-@click.option('-q', '--quiet', is_flag=True, help='Clear the prompt after accepting choice')
-@click.argument('options', nargs=-1)
-def select(multiple_choice, quiet, prompt, options):
-    choice = choose(prompt, options, multi=multiple_choice)
+def select(args):
+    choice = choose(args.prompt, args.OPTIONS, multi=args.multi)
     # if not quiet, print everything
-    if not quiet and prompt:
-        click.secho(prompt + ' ', err=True, nl=multiple_choice, bold=True)
+    if not args.quiet and args.prompt:
+        print(args.prompt + ' ', file=sys.stderr, end='\n' if args.multi else '')
     # if we're being piped to somewhere else, always print choice
-    if not quiet or not sys.stdout.isatty():
-        if multiple_choice:
-            # on multiple lines for multiple choices
-            click.echo('\n'.join(choice))
-        else:
-            click.echo(choice)
+    if not args.quiet or not sys.stdout.isatty():
+        print('\n'.join(choice))
 
 
-@cli.command()
-@click.option('-q', '--quiet', is_flag=True, help='Hide the prompt and response after accepting')
-@click.option('-y', '--default-true', is_flag=True, help='Expect true by default (if no choice is made)')
-@click.option('-p', '--prompt', default='Continue', help='The prompt to display')
-def confirm(default_true, quiet, prompt):
+def confirm(args):
     """Prompt user for a yes/no response"""
-    exit(not yesno.confirm(prompt, single_key=True, default=default_true, clear=quiet))
+    exit(not yesno.confirm(args.prompt or 'Continue? ', single_key=True, default=args.default_true, clear=args.quiet))
+
 
 if __name__ == "__main__":
-    cli()
+    parser = argparse.ArgumentParser(description='Take useful user input')
+    parser.add_argument('-p', '--prompt', help='Prompt to display')
+    parser.add_argument('-q', '--quiet',  action='store_true', help='Hide prompt after closing')
+
+    subparsers = parser.add_subparsers(dest='option')
+    select_parser = subparsers.add_parser('select', help='Select from multiple options')
+    select_parser.add_argument('-m', '--multi', action='store_true', help='Choose multiple options')
+    select_parser.add_argument('OPTIONS', nargs="+", help='Options to choose from')
+    select_parser.set_defaults(func=select)
+
+    free_parser = subparsers.add_parser('free', help="Get a roughly readline-ish prompt")
+    free_parser.set_defaults(func=free)
+
+    confirm_parser = subparsers.add_parser('confirm', help='Get a yes/no answer')
+    confirm_parser.add_argument('-t', '--default-true', action='store_true', help='Default to true instead of false')
+    confirm_parser.set_defaults(func=confirm)
+
+    args = parser.parse_args()
+    args.func(args)
